417cee251228261722460ba1a7c3322df62005ca
JYarab <jyarab@gmail.com>
2021-07-03 12:25:36 -0400

    added styling and DB query

--- users_app/views.py
+++ users_app/views.py
@@ -1,6 +1,15 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import redirect, render, HttpResponse
+from .models import User
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('Hello')
+    context = {
+    "users" : User.objects.all(),
+    "all_fields" : User._meta.get_fields()
+        }
+    return render(request, 'index.html', context)
+
+def add_user(request):
+    User.objects.create(first_name = request.POST['first_name'], last_name = request.POST['last_name'], email_address = request.POST['email'], age = request.POST['age'])
+    return redirect('/')
